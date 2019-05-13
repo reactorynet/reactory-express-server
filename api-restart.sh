9a2d69fcbe1c61ab5dac34e714471f38b4fe9f33
Werner Weber <werner.weber@gmail.com>
2019-05-13 11:39:22 +0200

    Run Config update TowerStone

--- api-restart.sh
+++ api-restart.sh
@@ -1,2 +1,15 @@
-pm2 stop 0
-pm2 start config/run/reactory.config.js
+#$0 - The name of the Bash script.
+#$1 - $9 - The first 9 arguments to the Bash script. (As mentioned above.)
+#$# - How many arguments were passed to the Bash script.
+#$@ - All the arguments supplied to the Bash script.
+#$? - The exit status of the most recently run process.
+#$$ - The process ID of the current script.
+#$USER - The username of the user running the script.
+#$HOSTNAME - The hostname of the machine the script is running on.
+#$SECONDS - The number of seconds since the script was started.
+#$RANDOM - Returns a different random number each time is it referred to.
+#$LINENO - Returns the current line number in the Bash script.
+$tenant=${1:-reactory}
+pm2 stop ${2:-0}
+git pull
+pm2 start config/run/$tenant.config.js
