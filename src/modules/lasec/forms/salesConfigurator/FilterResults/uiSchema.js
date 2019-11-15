ffe3a3ea53ec90341890ad1ad1ae610426ad487a
DrewZa <drewmurphyza@gmail.com>
2019-11-15 13:02:30 +0200

    Lasec Sales Config: Result list component

--- src/modules/lasec/forms/salesConfigurator/FilterResults/uiSchema.js
+++ src/modules/lasec/forms/salesConfigurator/FilterResults/uiSchema.js
@@ -1,34 +1,60 @@
 export default {
-  testCard: {
-    'ui:widget': 'MaterialCardWidget',
+  // testCard: {
+  //   'ui:widget': 'MaterialCardWidget',
+  //   'ui:options': {
+  //     component: 'lasec-crm.QuotesList',
+  //     componentProps: {
+  //       query: {
+  //         filterType: 'Recent'
+  //       }
+  //     },
+  //     actions: [
+  //       {
+  //         buttonType: 'button',
+  //         buttonText: 'MORE INFO',
+  //         buttonColor: 'default',
+  //         actionType: '',
+  //         actionHandler: ''
+  //       },
+  //       {
+  //         buttonType: 'button',
+  //         buttonText: 'ADD TO QUOTE',
+  //         buttonColor: 'primary',
+  //         actionType: '',
+  //         actionHandler: ''
+  //       },
+  //     ]
+  //   }
+  // },
+  filterResultList: {
+    'ui:widget': 'MaterialGridListWidget',
     'ui:options': {
-      component: {
-        componentFqn: 'lasec-crm.QuotesList',
+      contentType: 'widget',
+      componentFqn: 'MaterialCardWidget',
+      'ui:options': {
+        component: 'lasec-crm.QuotesList',
         componentProps: {
           query: {
             filterType: 'Recent'
           }
         },
-      },
-      actions: [
-        {
-          buttonType: 'button',
-          buttonText: 'MORE INFO',
-          buttonColor: 'default',
-          actionType: '',
-          actionHandler: ''
-        },
-        {
-          buttonType: 'button',
-          buttonText: 'ADD TO QUOTE',
-          buttonColor: 'primary',
-          actionType: '',
-          actionHandler: ''
-        },
-      ]
+        actions: [
+          {
+            buttonType: 'button',
+            buttonText: 'MORE INFO',
+            buttonColor: 'default',
+            actionType: '',
+            actionHandler: ''
+          },
+          {
+            buttonType: 'button',
+            buttonText: 'ADD TO QUOTE',
+            buttonColor: 'primary',
+            actionType: '',
+            actionHandler: ''
+          },
+        ]
+      }
     }
-  },
-  testList: {
-    'ui:widget': 'MaterialGridListWidget',
   }
 };
