3cb61653c0aea74e96e5d7b97bbc98e5dfb528f3
Werner Weber <werner.weber@gmail.com>
2019-12-17 07:30:10 +0200

    Backup script update

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # backup mongodb
-env=${1:-production}
-root=${2:-/data/reactory}
+db=${1:-reactory}
+env=${2:-production}
+root=${3:-/data/reactory}
 script_root=$(dirname $(readlink -f $0))
 filename=$(date +"%Y%m%d%H%M%S%N")
 if [ ! -d "$root/database/backup/$env" ]; then
   mkdir "$root/database/backup/$env"
 fi
-echo "Backing up to $root/database/backup/$env/reactory_db_backup_$filename.gz"
-mongodump --db reactory --gzip --archive > $root/database/backup/$env/reactory_db_backup_$filename.gz
+echo "Backing up to $root/database/backup/$env/$db_backup_$filename.gz"
+mongodump --db $db --gzip --archive > $root/database/backup/$env/$db_backup_$filename.gz
