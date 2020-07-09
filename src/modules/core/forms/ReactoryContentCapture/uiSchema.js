b7925cbf00896d1c955a7ae3944395f5b379b776
DrewZa <drewmurphyza@gmail.com>
2020-07-09 13:12:13 +0200

    Updated Reactory Content Capture form to have include a slightly extended froala option that takes file inputs

--- src/modules/core/forms/ReactoryContentCapture/uiSchema.js
+++ src/modules/core/forms/ReactoryContentCapture/uiSchema.js
@@ -31,6 +31,32 @@ const minmalOptions = {
     'moreParagraph': {
       'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
     },
+  },
+  fileUploadURL: '${formContext.api.API_ROOT}/froala/upload/file',
+  videoUploadURL: '${formContext.api.API_ROOT}/froala/upload/video',
+  imageUploadURL: '${formContext.api.API_ROOT}/froala/upload/image',
+  requestHeaders: {
+    'x-client-key': '${formContext.api.CLIENT_KEY}',
+    'x-client-pwd': '${formContext.api.CLIENT_PWD}',
+  },
+  quickInsertEnabled: false,
+};
+
+const minmalExtendedOptions = {
+  key: 'SDB17hB8E7F6D3eMRPYa1c1REe1BGQOQIc1CDBREJImD6F5E4G3E1A9D7C3B4B4==',
+  imageManagerLoadMethod: 'GET',
+  toolbarInline: false,
+  toolbarVisibleWithoutSelection: false,
+  imageDefaultWidth: 300,
+  imageDefaultDisplay: 'inline',
+  imageUploadMethod: 'POST',
+  toolbarButtons: 	{
+    'moreText': {
+      'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+    },
+    'moreParagraph': {
+      'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
+    },
     'moreRich': {
       'buttons': ['insertFile', 'insertLink', 'insertImage', 'insertVideo']
     },
@@ -44,8 +70,6 @@ const minmalOptions = {
   },
   quickInsertEnabled: false,
 };
-
-
 
 export const minimalEdit = {
   'ui:options': {
@@ -96,6 +120,86 @@ export const minimalEdit = {
     'ui:options': {
       showLabel: false,
       froalaOptions: minmalOptions,
+    },
+  },
+  published: {
+    'ui:widget': 'HiddenWidget',
+    'ui:options': {
+      style: {
+        display: 'none',
+        maxHeight: '0px',
+      }
+    },
+    readOnly: true,
+    hidden: true
+  },
+  topics: {
+  'ui:widget': 'HiddenWidget',
+  'ui:options': {
+    container: 'core.BasicContainer',
+    style: {
+      display: 'none',
+      maxHeight: '0px',
+    },
+    containerProps: {
+      title: 'Page Tags',
+      style: {
+        display: "none"
+      },
+    },
+  },
+  }
+};
+
+export const minimalExtendedEdit = {
+  'ui:options': {
+    containerType: "div",
+    containerStyles: {
+      padding: '0px',
+      margin: '0px',
+      paddingBottom: '8px'
+    },
+    style: {
+      marginTop: '16px',
+    },
+    showSubmit: true,
+    showRefresh: false,
+    showHelp: false,
+  },
+  title: {
+    'ui:widget': 'HiddenWidget',
+    'ui:options': {
+      showLabel: false,
+      style: {
+        display: 'none',
+        maxHeight: '0px',
+      }
+    }
+  },
+  slug: {
+    'ui:widget': 'HiddenWidget',
+    'ui:options': {
+      style: {
+        display: 'none',
+        maxHeight: '0px',
+      }
+    }
+  },
+  createdAt: {
+    'ui:widget': 'HiddenWidget',
+    'ui:options': {
+      showLabel: false,
+      style: {
+        display: 'none',
+        maxHeight: '0px',
+      }
+    }
+  },
+  content: {
+    'ui:widget': 'FroalaWidget',
+    'ui:options': {
+      showLabel: false,
+      froalaOptions: minmalExtendedOptions,
     },
   },
   published: {
