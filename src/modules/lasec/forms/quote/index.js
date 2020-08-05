496fb1712dd0067960a5534fa9f13f50bf9fa4db
DrewZa <drewmurphyza@gmail.com>
2020-08-05 10:57:58 +0200

    Removed references to old QuoteDuplicate screen

--- src/modules/lasec/forms/quote/index.js
+++ src/modules/lasec/forms/quote/index.js
@@ -3,6 +3,5 @@ export { default as QuoteIdInputForm } from './QuoteIdInputForm';
 export { default as QuoteDetailForm } from './QuoteDetail';
 export { default as QuoteListForm } from './QuoteList';
 export { default as QuoteEmail } from './QuoteEmail';
-export { default as QuoteDuplicate } from './QuoteDuplicate';
 export { default as QuoteDuplicateClientSelector } from './QuoteDuplicateClientSelector';
 export { default as NewQuoteClientSelector } from './NewQuoteClientSelector';
