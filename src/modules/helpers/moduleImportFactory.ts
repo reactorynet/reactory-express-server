fcf2f7ccac9a24ab660741cb652b1f3a7242e9fe
Werner Weber <werner.weber@gmail.com>
2021-08-30 08:04:56 +0200

    gitignore edit
    Added moduleImportFactory.ts

---
+++ src/modules/helpers/moduleImportFactory.ts
@@ -0,0 +1,89 @@
+/**
+ * Responsible for generating the __index.ts
+ * imports file for the modules we want to run.
+ */
+import fs from 'fs';
+import { Reactory } from '@reactory/server-core/types/reactory';
+import logger from '@reactory/server-core/logging';
+
+const getImportName = (moduleDefinition: Reactory.IReactoryModuleDefinition) => {
+
+  let $import_name = moduleDefinition.fqn.replace(/\./g, '_',);
+  $import_name = $import_name.replace(/-/, '__');
+  $import_name = $import_name.replace('@', '_VER_');
+
+  return $import_name;
+};
+
+/**
+ * Generates __index.ts that is responsible for module includes.
+ */
+const generate_index = () => {
+
+  const { MODULES_ENABLED } = process.env;
+
+  logger.debug(`♻ ModuleImportFactory >> __index.ts generator executing for module spec ${MODULES_ENABLED || 'NA'}`);
+
+  const filename = `${MODULES_ENABLED}.json`
+  let enabled: Reactory.IReactoryModuleDefinition[] = []
+  if (fs.existsSync(`./src/modules/${filename}`) === true) {
+    enabled = require(`../${filename}`);
+  } else {
+    logger.error(`The module specification file (./src/modules/${filename}) does not exist, please create it and restart the server`);
+    process.exit(0);
+  }
+
+  logger.debug(`Found ${enabled.length} module definintions in ${filename}`);
+
+  let file_contents = `
+/**
+ * This file is generated with each startup, do not modify or check into the repo.
+ * See the readme.md in the modules folder for more details.
+ * */
+"use strict";`;
+
+  let module_names = '';
+  enabled.forEach((moduleDefinition: Reactory.IReactoryModuleDefinition, midx: number) => {
+    logger.debug(`Enabling Reactory Module ⚙ [${moduleDefinition.name}]`);
+    // eslint-disable-next-line global-require
+
+    let import_name = getImportName(moduleDefinition);
+
+    file_contents = `${file_contents}
+
+/**
+ * Generated import for module id ${moduleDefinition.id}
+ * */ 
+import ${import_name} from '@reactory/server-modules/${moduleDefinition.moduleEntry.replace('.ts', '').replace('.js', '')}';`;
+    module_names = midx === 0 ? `\t${import_name}` : `${module_names},\n\t${import_name}`;
+  });
+
+
+  file_contents = `${file_contents}
+  
+export default [
+${module_names}
+];
+
+`;
+
+  const __index = './src/modules/__index.ts';
+  let do_write = false;
+  let file_exists = fs.existsSync(__index);
+  if (file_exists === true) {
+    const existing = fs.readFileSync(__index, { encoding: 'utf8' });
+    do_write = existing !== file_contents;
+  } else {
+    do_write = true;
+  }
+
+
+  if (do_write === true) {
+    if (file_exists === true) {
+      fs.unlinkSync(__index);
+    }
+    fs.writeFileSync(__index, file_contents, { encoding: 'utf8' });
+  }
+};
+
+export default generate_index;
