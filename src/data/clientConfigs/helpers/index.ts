4faa5dc945edc3e90590746783abce4b8998dae9
Werner Weber <werner.weber@gmail.com>
2021-08-30 08:16:29 +0200

    Edit gitignore and added config factory

---
+++ src/data/clientConfigs/helpers/index.ts
@@ -0,0 +1,4 @@
+export { default as ClientConfigBuilder } from './configBuilder';
+export { default as ClientDefaultRoutes } from './defaultRoutes';
+export { default as ConfigImportFactory } from './configImportFactory';
+export { default as DefaultMenus } from './menus';
