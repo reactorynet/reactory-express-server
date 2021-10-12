06bd05fa3ffcde42eb3d970299677f210d635e36
Werner Weber <werner.weber@gmail.com>
2021-10-12 13:59:33 +0200

    added Content.ts

---
+++ src/models/schema/Content.ts
@@ -0,0 +1,37 @@
+import mongoose, { mongo } from 'mongoose';
+import { Reactory } from '@reactory/server-core/types/reactory';
+const { ObjectId } = mongoose.Schema.Types;
+
+
+const ContentSchema = new mongoose.Schema<Reactory.IReactoryContent>({
+  id: ObjectId,
+  slug: String,
+  client: {
+    type: ObjectId,
+    ref: 'ReactoryClient'
+  },
+  topics: [ String ],
+  title: String,
+  content: String,
+  template: Boolean,
+  engine: String,
+  previewInputForm: String,
+  description: String,
+  helpTopic: String,
+  version: String,
+  createdAt: Date,
+  updatedAt: Date,
+  createdBy: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  updatedBy: {
+    ref: 'User',
+    type: ObjectId,
+  },
+  published: Boolean,
+  comments: [ ObjectId ]
+});
+
+const ContentModel = mongoose.model<Reactory.IReactoryContentDocument>('Content', ContentSchema);
+export default ContentModel;
