2daba1076872faa16d04bad0733e1ccefa684a4d
Werner Weber <werner.weber@gmail.com>
2021-10-14 07:06:30 +0200

    Updates top ReactoryContent graph Fix global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,20 +1,10 @@
-// import { Reactory } from "./reactory";
-
-declare type IUser = import("@reactory/server-core/types/reactory").Reactory.IUser
-declare type IPartner = import("@reactory/server-core/types/reactory").Reactory.IPartner
 /**
  * IMPORTANT - do not use imports in this file!
  * It will break global definition.
  */
 declare namespace NodeJS {
   export interface Global {
-    user: IUser;
-    partner: IPartner;
     REACTORY_SERVER_STARTUP: Date;
-    getService: Function;
   }
 }
-declare var user: IUser;
-declare var partner: IPartner;
-declare var getService: Function;
  
