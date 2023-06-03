4dffde56f26892558a6adbc8d4925ef393f5ff7e
Werner Weber <werner.weber@gmail.com>
2023-06-03 02:18:25 +0200

    Minor babel tweak

--- babel.config.js
+++ babel.config.js
@@ -44,6 +44,8 @@ module.exports = function (api) {
     }],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
 
     // stage 0
     '@babel/plugin-proposal-function-bind',
